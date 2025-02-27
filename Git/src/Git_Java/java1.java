02e9749fb2f18243f9d7de521f48d2f688c2d66c
AarinKPMG <aarinsheikm@kpmg.com>
2025-02-27 10:44:22 +0530

    first java code

---
+++ Git/src/Git_Java/java1.java
@@ -0,0 +1,9 @@
+package Git_Java;
+
+public class java1 {
+	
+	public static void main(String[] args) {
+		System.out.println("This is my first Java code that is about to Commit for the first time");
+	}
+	
+}
